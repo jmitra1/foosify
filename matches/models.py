2c94834dc18ed125090a926c3ec68d8594d1c8e3
Sheldon Ketterer <smketterer@gmail.com>
2016-05-18 21:52:00 -0600

    Fix Trueskill ranking to work for new matches (when you first select winners and losers).

--- matches/models.py
+++ matches/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
-from django.db.models.signals import post_save
+from django.db.models.signals import m2m_changed
+from django.dispatch import receiver
 from players.models import Player
 import datetime
 import trueskill
@@ -25,39 +26,32 @@ class Match(models.Model):
 			string += "<a href='@" + player["slug"] + "'>"
 			string += player["slug"]
 			string += "</a>"
-
-		#return ", ".join(self.players.values_list("user__username", flat=True))
 		return string
 
 	def update_player_skill_values(self):
-		print "\n Skill updated! \n"
-		# Get the winner rating objects and store them in a tuple.
 		winner_ratings = []
 		for winner in self.winners.all():
 			winner_ratings.append(
 				trueskill.Rating(winner.rating_mu, winner.rating_sigma)
 			)
-		winner_ratings = tuple(winner_ratings,)
-		print winner_ratings
+		winner_ratings = tuple(winner_ratings)
 
-		# Get the loser rating objects and store them in a tuple.
 		loser_ratings = []
 		for loser in self.losers.all():
 			loser_ratings.append(
 				trueskill.Rating(loser.rating_mu, loser.rating_sigma)
 			)
-		loser_ratings = tuple(loser_ratings,)
+		loser_ratings = tuple(loser_ratings)
 
-		# Then get the new ratings from the match.
 		match_rating_result = trueskill.rate([winner_ratings, loser_ratings])
 
-		# Then update the winners and losers based on the returned ratings.
 		i = 0
 		for winner in self.winners.all():
 			winner.rating_mu = match_rating_result[0][i].mu
 			winner.rating_sigma = match_rating_result[0][i].sigma
 			winner.save(update_fields=["rating_mu", "rating_sigma"])
 			i += 1
+
 		i = 0
 		for loser in self.losers.all():
 			loser.rating_mu = match_rating_result[1][i].mu
@@ -65,16 +59,22 @@ class Match(models.Model):
 			loser.save(update_fields=["rating_mu", "rating_sigma"])
 			i += 1
 
-	def save(self, *args, **kwargs):
-		# Check if self exists before attempting to update, so as to only update
-		# skill values on the second save, and to have players to update.
-		super(Match, self).save()
-		if self.pk is not None and self.winners.count() > 0 and self.losers.count() > 0:
-			self.update_player_skill_values()
-
 	class Meta:
 		verbose_name_plural = "matches"
 		ordering = ["-id"]
 
 	def __unicode__(self):
 		return unicode(self.id)
+
+@receiver(m2m_changed)
+def update_rankings_on_m2m_change(sender, instance, action, **kwargs):
+	# Calculating the ranking changes for each match requires the M2M
+	# relationships to already be added, which occurs after save (even
+	# post_save). This event will fire a few times: once when the site is first
+	# created and the players selected, and twice on update (for winners and
+	# losers). This will work when the match winners/losers are first selected,
+	# however, if you edit a match this will double count the rating changes.
+	print sender, instance, action
+	if action == "post_add":
+		if instance.winners.count() > 0 and instance.losers.count() > 0:
+			instance.update_player_skill_values()
